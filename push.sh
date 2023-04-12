20d23a4ac48486d40be81f7ace4ba19dfc8dac15
Henzolina <henokrobale@gmail.com>
2023-04-12 12:00:22 -0500

    initial_commit

---
+++ push.sh
@@ -0,0 +1,49 @@
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Henzolena/CV_Bootstrap.git
+git push -u origin main
+
+#!/bin/bash
+
+# Stage changes
+git add .
+
+# Commit changes with message
+git commit -m "initial_commit"
+
+#choose a branch
+git branch -M main
+
+#select a repository
+git remote add origin https://github.com/Henzolena/CV_Bootstrap.git
+
+# Push changes to remote repository
+git push origin master
+
+
+
+#!/bin/bash
+
+# Define variables
+COMMIT_MSG="initial_commit"
+LOG_FILE="push_changes.log"
+
+# Redirect all output to log file
+exec &>> $LOG_FILE
+
+# Check if any changes are staged
+if ! git diff --quiet --cached; then
+    # Commit changes with message
+    git commit -m "$COMMIT_MSG"
+
+    # Push changes to remote repository
+    if git push origin master; then
+        echo "Changes pushed successfully"
+    else
+        echo "Failed to push changes"
+    fi
+else
+    echo "No changes to push"
+fi
